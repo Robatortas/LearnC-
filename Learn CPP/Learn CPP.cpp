ef2a9b7758b98e647ca4205e6c5dded8eb25f024
Robatortas <robatortas1@gmail.com>
2022-08-23 22:20:39 -0700

    Practice 2

--- Learn CPP/Learn CPP.cpp
+++ Learn CPP/Learn CPP.cpp
@@ -3,26 +3,14 @@
 
 // STD => SHORT FOR STANDARD LIBRARY
 
-// << is for input. || >> is for output! (at least in the std stuff)
+// << is for input. || >> is for output! (at least in the std stuff
 
 int main() {
-	// character out => cout
-	std::cout << "Hello World!";
-
-	/* 
-	* Zero tells the operating system that the program is gonna terminate correctly.
-	* If it returns any other values, it means an error occurred!
-	*/
+	double x = 10;
+	double y = 5;
+	double z = (x + 10) / (3 * y);
+	// (STREAM INSERTION OPERATOR) << inserting something to the output stream(cout) basically.
+	// ENDL is the end of the line... \n basically.
+	std::cout << "x = " << x << std::endl << "y = " << y;
 	return 0;
 }
-
-
-// Tut stuff I'll use later!
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
